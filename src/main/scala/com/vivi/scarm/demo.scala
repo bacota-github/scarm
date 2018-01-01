f1ad1d81174f128ceabd6e55d1cec38386cdb645
Bruce Cota <bcota@unicon.net>
2017-12-31 22:41:40 -0500

    demo using Composite

--- src/main/scala/com/vivi/scarm/demo.scala
+++ src/main/scala/com/vivi/scarm/demo.scala
@@ -4,8 +4,9 @@ import doobie.imports._
 import cats.effect.IO
 import doobie.util.transactor.Transactor
 import java.util.UUID
-   
+
 object Demo {
+
   def main: Unit = {
     val xa = Transactor.fromDriverManager[IO](
       //        "org.h2.Driver", "jdbc:h2:mem:db" , "" , ""
@@ -18,7 +19,7 @@ object Demo {
       sql"""create table if not exists people
           ( id char(36) not null primary key
           , name varchar(512) not null
-          , email varchar(512) not null unique
+          , email varchar(512) not null 
           )
      """.update.run
 
@@ -28,7 +29,7 @@ object Demo {
       sql"""create table if not exists course
           ( id char(36) not null primary key
           , name varchar(512) not null
-          , teacherId varchar(36)
+          , teacherId varchar(36) references people
           )
      """.update.run
 
@@ -96,5 +97,74 @@ object Demo {
 
     val l2 = run(getCourse)
     l2.foreach { case c => println(s"${c}") }
+
+    case class CourseWithTeacher(course: Course, teacher: Person)
+    def getCoursesWithTeacher: ConnectionIO[List[CourseWithTeacher]] = 
+      (sql"""select co.*, p.*
+         from course co 
+         inner join people p on co.teacherId = p.id
+      """.query[CourseWithTeacher]
+      ).list
+
+    run(getCoursesWithTeacher).foreach { case co => println(s"${co}") }
+
+    case class TeacherWithCourses(teacher: Person, courses: Course)
+    def getTeacherWithCourses: ConnectionIO[List[TeacherWithCourses]] = 
+      (sql"""select p.*, co.*
+         from people p
+         inner join course co on co.teacherId = p.id
+      """.query[TeacherWithCourses]
+      ).list
+    println(run(getTeacherWithCourses))
+
+    def getPeopleT: ConnectionIO[List[(Person)]] =
+      (sql"""select id, name, email
+         from people
+      """.query[(Person)]
+      ).list
+
+    println("getPeopleT")
+    println(run(getPeopleT))
+
+    def getTeacherWithCoursesT: ConnectionIO[List[(Person,Course)]] = 
+      (sql"""select p.*, co.*
+         from people p
+         inner join course co on co.teacherId = p.id
+      """.query[(Person,Course)]
+      ).list
+    println(run(getTeacherWithCoursesT))
+
+    def getTeacherWithCoursesT2: ConnectionIO[List[(Person,Course,Person)]] =
+      (sql"""select p.*, co.*, p2.*
+         from people p
+         inner join course co on co.teacherId = p.id
+         inner join people p2 on co.teacherId = p2.id
+      """.query[(Person,Course,Person)]
+      ).list
+    println(run(getTeacherWithCoursesT2))
+
+
+//      Meta[(Option[String], Option[String], Option[String])].xmap(
+    implicit lazy val optionalCourseComposite: Composite[Option[Course]] =
+      //      Composite[(Option[String], Option[String], Option[String])].xmap(
+      Composite[(Option[String], Option[String], Option[String])].imap(
+        (t: (Option[String], Option[String], Option[String])) =>
+        t._1 match {
+          case None => None
+          case Some(t1) => Some(Course(UUID.fromString(t1), t._2.get, UUID.fromString(t._3.get)))
+        })(
+        (t: Option[Course]) => t match {
+          case None => (None, None, None)
+          case Some(co) => (Some(co.id.toString), Some(co.name), Some(co.teacherId.toString))
+        }
+      )
+
+    def getTeacherWithCoursesOptT: ConnectionIO[List[(Person,Option[Course])]] =
+      (sql"""select p.*, co.*
+         from people p
+         left outer join course co on co.teacherId = p.id
+      """.query[(Person,Option[Course])]
+      ).list
+    println(run(getTeacherWithCoursesOptT))
   }
 }
