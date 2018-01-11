61681e46355bab6ea91a27a89aa0d8ad278649c3
Bruce Cota <bcota@unicon.net>
2018-01-11 10:15:17 -0500

    dsl fixed to support some examples

--- src/main/scala/com/vivi/scarm/dsl.scala
+++ src/main/scala/com/vivi/scarm/dsl.scala
@@ -6,6 +6,8 @@ import fs2.Stream
 
 import scala.util.{Failure,Success,Try}
 
+import scala.language.higherKinds
+
 sealed trait DatabaseObject {
   def create: Update0
   def drop: Update0
@@ -16,13 +18,17 @@ sealed trait Queryable[K, T] {
   def query(key: K): Stream[ConnectionIO,T]
 }
 
+trait Entity[K] {
+  type KEY=K
+  def id: K
+}
 
-case class Table[K,E](
-  name: String,
-  id: E => K
+case class Table[K,E<:Entity[K]](
+  name: String
 ) extends DatabaseObject with Queryable[K,E] {
   override def create: Update0 = null
   override def drop: Update0 = null
+  lazy val primaryKey = Index[K, E](name+"_pk", this, (e: E) => e.id)
   override def query(key: K): Stream[ConnectionIO,E] = Stream()
 // def fetch(key: K): ConnectionIO[Option[E]] =  
 //    query(key).head
@@ -56,52 +62,55 @@ case class Index[K,E](
 
 
 case class ForeignKey[FROM,PK,TO](
-  name: String,
   from: Table[_,FROM],
-  to: Table[PK,TO],
-  fromKey: FROM => PK
+  fromKey: FROM => PK,
+  to: Table[PK,TO]
 ) extends DatabaseObject {
   override def create: Update0 = null
   override def drop: Update0 = null
-  lazy val manyToOne = ManyToOne(to, fromKey)
-  lazy val oneToMany = OneToMany(to, fromKey)
+  override lazy val name = s"${from.name}_${to.name}_fk"
+  lazy val manyToOne = ManyToOne(to, fromKey, to.primaryKey.id)
+  lazy val oneToMany = OneToMany[TO,PK,FROM](index, to.primaryKey.id, fromKey)
   lazy val index = Index(name+"_ix", from, fromKey)
 }
 
 case class OptionalForeignKey[PK,FROM,TO](
-  name: String,
   from: Table[_,FROM],
-  to: Table[PK,TO],
-  fromKey: FROM => PK
+  fromKey: FROM => PK,
+  to: Table[PK,TO]
 ) extends DatabaseObject {
   override def create: Update0 = null
   override def drop: Update0 = null
-  lazy val manyToOne = OptionalManyToOne(to, fromKey)
-  lazy val oneToMany = OneToMany(to, fromKey)
+  override lazy val name = s"${from.name}_${to.name}_fk"
+  lazy val manyToOne = OptionalManyToOne(to, fromKey, to.primaryKey.id)
+  lazy val oneToMany = OneToMany[TO,PK,FROM](index, to.primaryKey.id, fromKey)
   lazy val index = Index(name+"_ix", from, fromKey)
 }
 
 
 case class ManyToOne[FROM,PK,TO](
   to: Queryable[PK,TO],
-  fromKey: FROM => PK
+  fromKey: FROM => PK,
+  toKey: TO => PK
 ) {
   def join[K](q: Queryable[K,FROM]): Queryable[K, (FROM,TO)] =
     ManyToOneJoin(q, this)
   def ::[K](q: Queryable[K,FROM]) = join(q)
 
   def join[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2,PK2,(FROM,TO)] =
-    ManyToOne(join(m21.to), m21.fromKey)
+    ManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2,PK2,(FROM,TO)] =
     join(m21)
 
   def join[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
-      :OptionalManyToOne[FROM2,PK2,(FROM,TO)] =  OptionalManyToOne(join(m21.to), m21.fromKey)
+      :OptionalManyToOne[FROM2,PK2,(FROM,TO)]
+    = OptionalManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
       :OptionalManyToOne[FROM2,PK2,(FROM,TO)] =  join(m21)
 
   def join[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
-      :OneToMany[FROM2,PK2,(FROM,TO)] = OneToMany(join(o2m.to), o2m.fromKey)
+      :OneToMany[FROM2,PK2,(FROM,TO)]
+  = OneToMany(join(o2m.to), o2m.fromKey, p => o2m.toKey(p._1))
   def ::[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
       :OneToMany[FROM2,PK2,(FROM,TO)] = join(o2m)
 }
@@ -116,25 +125,27 @@ case class ManyToOneJoin[K,FROM,PK,TO](
 
 case class OptionalManyToOne[FROM,PK,TO](
   to: Queryable[PK,TO],
-  fromKey: FROM => PK
+  fromKey: FROM => PK,
+  toKey: TO => PK
 ) {
   def join[K](q: Queryable[K,FROM]): Queryable[K, (FROM,Option[TO])]
      = OptionalManyToOneJoin(q, this)
   def ::[K](q: Queryable[K,FROM]) = join(q)
 
   def join[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2, PK2,(FROM,Option[TO])] =
-    ManyToOne(join(m21.to), m21.fromKey)
+    ManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2,PK2,(FROM,Option[TO])] =
     join(m21)
 
   def join[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
       :OptionalManyToOne[FROM2,PK2,(FROM,Option[TO])]
-  = OptionalManyToOne(join(m21.to), m21.fromKey)
+  = OptionalManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
       :OptionalManyToOne[FROM2,PK2,(FROM,Option[TO])] = join(m21)
 
   def join[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
-      :OneToMany[FROM2,PK2,(FROM,Option[TO])] = OneToMany(join(o2m.to), o2m.fromKey)
+      :OneToMany[FROM2,PK2,(FROM,Option[TO])]
+  = OneToMany(join(o2m.to), o2m.fromKey, p => o2m.toKey(p._1))
   def ::[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
       :OneToMany[FROM2,PK2,(FROM,Option[TO])] = join(o2m)
 }
@@ -146,28 +157,29 @@ case class OptionalManyToOneJoin[K,FROM,PK,TO](
   override def query(key: K): Stream[ConnectionIO,(FROM,Option[TO])] = Stream()
 }
 
-
 case class OneToMany[FROM,PK,TO](
   to: Queryable[PK,TO],
-  fromKey: FROM => PK
+  fromKey: FROM => PK,
+  toKey: TO => PK
 ) {
   def join[K](q: Queryable[K,FROM]): Queryable[K, (FROM,Set[TO])] =
     OneToManyJoin(q,this)
   def ::[K](q: Queryable[K,FROM]) = join(q)
 
   def join[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2, PK2,(FROM,Set[TO])] =
-    ManyToOne(join(m21.to), m21.fromKey)
+    ManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: ManyToOne[FROM2,PK2,FROM]): ManyToOne[FROM2,PK2,(FROM,Set[TO])] =
     join(m21)
 
   def join[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
       :OptionalManyToOne[FROM2,PK2,(FROM,Set[TO])]
-  = OptionalManyToOne(join(m21.to), m21.fromKey)
+  = OptionalManyToOne(join(m21.to), m21.fromKey, p => m21.toKey(p._1))
   def ::[FROM2,PK2](m21: OptionalManyToOne[FROM2,PK2,FROM])
       :OptionalManyToOne[FROM2,PK2,(FROM,Set[TO])] = join(m21)
 
   def join[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
-      :OneToMany[FROM2,PK2,(FROM,Set[TO])] = OneToMany(join(o2m.to), o2m.fromKey)
+      :OneToMany[FROM2,PK2,(FROM,Set[TO])]
+  = OneToMany(join(o2m.to), o2m.fromKey, p => o2m.toKey(p._1))
   def ::[FROM2,PK2](o2m:OneToMany[FROM2,PK2,FROM])
       :OneToMany[FROM2,PK2,(FROM,Set[TO])] = join(o2m)
 }
---
+++ src/main/scala/com/vivi/scarm/dslTest.scala
@@ -0,0 +1,69 @@
+package com.vivi.scarm
+
+import java.util.Calendar
+import doobie.ConnectionIO
+import fs2.Stream
+
+case class TeacherId(id: Int) extends AnyVal
+case class CourseId(id: Int) extends AnyVal
+case class StudentId(id: Int) extends AnyVal
+case class SectionId(courseId: CourseId, semester: Int, number: Int)
+case class EnrollmentId(studentId: StudentId, sectionId: SectionId)
+
+case class Teacher(id: TeacherId, name: String) extends Entity[TeacherId]
+case class Course(id: CourseId, subject: String, prerequisite: CourseId) extends Entity[CourseId]
+
+case class Section(sectionId: SectionId, instructor: TeacherId, 
+  room: String, time: Calendar) extends Entity[SectionId]
+
+case class Student(id: StudentId, name: String, level: Int)
+    extends Entity[StudentId]
+
+case class Enrollment(enrollmentId: EnrollmentId, grade: Option[Char])
+    extends Entity[EnrollmentId]
+
+
+class DSLTest {
+  val teachers = Table[TeacherId,Teacher]("teachers")
+  val courses = Table[CourseId,Course]("courses")
+  val sections = Table[SectionId,Section]("sections")
+  val students = Table[StudentId,Student]("students")
+  val enrollments = Table[EnrollmentId, Enrollment]("enrollments")
+
+  val sectionsBySemester = Index("sectionsBySemester", sections,
+    (s: Section) =>  s.id.semester
+  )
+
+  val instructor = ForeignKey(sections,(s: Section) => s.instructor, teachers)
+  val prerequisite = OptionalForeignKey(courses, (c: Course) => c.prerequisite, courses)
+  val sectionCourse = ForeignKey(sections, (s: Section) => s.id.courseId, courses)
+  val enrollmentCourse = ForeignKey(enrollments, (e: Enrollment) => e.id.sectionId, sections)
+  val enrollmentStudent = ForeignKey(enrollments, (e: Enrollment) => e.id.studentId, students)
+
+  val teacher: Stream[ConnectionIO,Teacher]  = teachers.query(TeacherId(1))
+
+  val courseWithTeacher: Stream[ConnectionIO, (Section,Teacher)] =
+    (sections :: instructor.manyToOne).query(SectionId(CourseId(1), 1, 1))
+
+  val teacherWithSections: Stream[ConnectionIO, (Teacher,Set[Section])] =
+    (teachers :: instructor.oneToMany).query(TeacherId(1))
+
+  val teacherWithSectionsAndStudents
+      :Stream[ConnectionIO, (Teacher,Set[(Section,Set[(Enrollment, Student)])])]
+    (teachers :: instructor.oneToMany :: enrollmentCourse.oneToMany ::
+      enrollmentStudent.manyToOne).query(TeacherId(1))
+
+  val teacherWithCourses: Stream[ConnectionIO, (Teacher,Set[(Section,Course)])] =
+    (teachers :: instructor.oneToMany :: sectionCourse.manyToOne).query(TeacherId(1))
+
+  val foo = instructor.oneToMany :: sectionCourse.manyToOne
+
+/*    val teacherWithCoursesAndStudents: Stream[ConnectionIO, (Teacher,Set[((Section,Course), Set[(Enrollment,Student)])])] =
+      (teachers :: (instructor.oneToMany ::sectionCourse.manyToOne)
+          :: 
+      ).query(TeacherId(1))
+ */
+
+
+}
+
