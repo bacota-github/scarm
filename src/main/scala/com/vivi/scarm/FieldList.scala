b0eaeaabb64f64b59decd0fdca69a73d735f3de1
Bruce Cota <bcota@unicon.net>
2018-03-28 13:41:47 -0400

    some simplification of FieldList

--- src/main/scala/com/vivi/scarm/FieldList.scala
+++ src/main/scala/com/vivi/scarm/FieldList.scala
@@ -16,17 +16,17 @@ trait FieldList[A] {
 trait FieldListLowPriority {
   implicit def primitiveFieldList[K <: Symbol, H, T <: HList](implicit
     witness: Witness.Aux[K],
-    tLister: FieldList[T]
+    tList: FieldList[T]
   ): FieldList[FieldType[K, H] :: T] =
     new FieldList[FieldType[K, H] ::T] {
-      override val names = witness.value.name :: tLister.names
+      override val names = witness.value.name :: tList.names
     }
 }
 
 object FieldList extends FieldListLowPriority {
 
-  def apply[T](implicit fieldLister: FieldList[T]): FieldList[T] =
-    fieldLister
+  def apply[T](implicit fieldList: FieldList[T]): FieldList[T] =
+    fieldList
 
   implicit def make[A,ARepr<:HList](
     implicit gen: LabelledGeneric.Aux[A, ARepr],
@@ -34,24 +34,24 @@ object FieldList extends FieldListLowPriority {
   ): FieldList[A] = new FieldList[A] {
     override val names = generator.names
   }
-
-  def genericLister[A, R](implicit
+/*
+  def genericList[A, R](implicit
     gen: LabelledGeneric.Aux[A, R],
     lister: Lazy[FieldList[R]]
   ): FieldList[A] = new FieldList[A] {
     override val names = lister.value.names
   }
-
-  implicit val hnilLister: FieldList[HNil] = new FieldList[HNil] {
+ */
+  implicit val hnilList: FieldList[HNil] = new FieldList[HNil] {
     override val names = Nil
   }
 
-  implicit def hconsLister[K, H, T <: HList](implicit
-    hLister: Lazy[FieldList[H]],
-    tLister: FieldList[T]
+  implicit def hconsList[K, H, T <: HList](implicit
+    hList: Lazy[FieldList[H]],
+    tList: FieldList[T]
   ): FieldList[FieldType[K, H] :: T] =
     new FieldList[FieldType[K, H] :: T] {
-      override val names = hLister.value.names ++ tLister.names
+      override val names = hList.value.names ++ tList.names
     }
 }
 
