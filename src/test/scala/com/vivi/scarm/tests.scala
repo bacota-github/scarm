40b4eb8d034c1a52ffad3b392de4a9066b039bcf
Bruce Cota <bcota@unicon.net>
2018-05-15 15:20:20 -0400

    unit tests for composite keys

--- src/test/scala/com/vivi/scarm/tests.scala
+++ src/test/scala/com/vivi/scarm/tests.scala
@@ -132,7 +132,14 @@ case class DSLTest(driver: String,
     if (cleanup(xa))  dropAll(xa) else ()
   }
 
+  private val rand = new java.util.Random()
   private def randomString: String = java.util.UUID.randomUUID().toString
+  private def randomCompositeKey = {
+    val first = rand.nextLong()
+    val last = randomString
+    val innerKey = InnerKey(rand.nextInt().toShort, rand.nextInt().toShort)
+    CompositeKey(first, innerKey, last)
+  }
 
   test("After inserting an entity into a table with String primary key, the entity can be selected")  {
     val e1 = StringKeyEntity(StringId(randomString), randomString)
@@ -219,7 +226,7 @@ case class DSLTest(driver: String,
     }
   }
 
-  test("after deleting by String primary, selecting on those keys returns None") {
+  test("after deleting by String primary key, selecting on those keys returns None") {
     val e1 = StringKeyEntity(StringId(randomString), randomString)
     val e2 = StringKeyEntity(StringId(randomString), randomString)
     val e3 = StringKeyEntity(StringId(randomString), randomString)
@@ -352,7 +359,6 @@ case class DSLTest(driver: String,
     assert(run(primitiveTable(e3.id)) == Some(e3))
   }
 
-
   test("updates of entities with primitive are reflected in future selects") {
     val e1 = PrimitiveEntity(Id(nextId), randomString)
     val e2 = PrimitiveEntity(Id(nextId), randomString)
@@ -369,14 +375,120 @@ case class DSLTest(driver: String,
     assert(run(primitiveTable(e3.id)) == Some(e3))
   }
 
-test("After inserting an entity into a table with composite primary key, the entity can be selected") (pending)
-  test("After inserting a batch of entities into a table with composite primary key, every entity can be selected") (pending)
-  test("insertReturningKey of an entity with composite primary key returns the correct Key and the entity can be selected") (pending)
-  test("insertBatchReturningKey on entities with composite primary key returns the correct Keys and the entities can be selected") (pending)
-  test("insertReturning an entity with composite primary key returns the entity and the entity can be selected") (pending)
-  test("insertBatchReturning entities with composite primary key returns the entities and the entities can be selected") (pending)
-  test("after deleting by composite primary, selecting on those keys returns None") (pending)
-  test("updates of entities with composite primary key are reflected in future selects") (pending)
+
+  test("After inserting an entity into a table with composite key, the entity can be selected")  {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    run(for {
+      i1 <- compositeTable.insert(e1)
+      i2 <- compositeTable.insert(e2)
+      i3 <- compositeTable.insert(e3)
+      e2New <- compositeTable(e2.id)
+      e1New <- compositeTable(e1.id)
+      e3New <- compositeTable(e3.id)
+    } yield {
+      assert (i1 == 1)
+      assert (i2 == 1)
+      assert (i3 == 1)
+      assert(e1New == Some(e1))
+      assert(e2New == Some(e2))
+      assert(e3New == Some(e3))
+    })
+  }
+
+  test("After inserting a batch of entities into a table with composite key, every entity can be selected") {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    run(for {
+      i <- compositeTable.insertBatch(e1,e2,e3)
+      e2New <- compositeTable(e2.id)
+      e1New <- compositeTable(e1.id)
+      e3New <- compositeTable(e3.id)
+    } yield {
+      assert (i == 3)
+      assert(e1New == Some(e1))
+      assert(e2New == Some(e2))
+      assert(e3New == Some(e3))
+    })
+  }
+
+  test("insertReturningKey of an entity with composite key returns the correct Key and the entity can be selected") {
+    val e = CompositeKeyEntity(randomCompositeKey, randomString)
+    run(for {
+      k <- compositeTable.insertReturningKey(e)
+      eNew <- compositeTable(k)
+    } yield {
+      assert (k == e.id)
+      assert(eNew == Some(e))
+    })
+  }
+
+
+  test("insertBatchReturningKey on entities with composite key returns the correct Keys and the entities can be selected") {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val entities = Seq(e1,e2,e3)
+    val keys = run(compositeTable.insertBatchReturningKeys(e1,e2,e3))
+    assert(keys == entities.map(_.id))
+    for (e <- entities) {
+      assert(run(compositeTable(e.id)) == Some(e))
+    }
+  }
+
+  test("insertReturning an entity with composite key returns the entity and the entity can be selected") {
+    val e = CompositeKeyEntity(randomCompositeKey, randomString)
+    run(for {
+      returned <- compositeTable.insertReturning(e)
+      selected <- compositeTable(e.id)
+    } yield {
+      assert(returned == e)
+      assert(selected == Some(e))
+    })
+  }
+
+  test("insertBatchReturning entities with composite key returns the entities and the entities can be selected") {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val entities = Seq(e1,e2,e3)
+    val returned = run(compositeTable.insertBatchReturning(e1,e2,e3))
+    assert(returned == entities)
+    for (e <- entities) {
+      assert(run(compositeTable(e.id)) == Some(e))
+    }
+  }
+
+  test("after deleting by composite key, selecting on those keys returns None") {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    assert(run(compositeTable.insertBatch(e1,e2,e3)) == 3)
+    assert(run(compositeTable.delete(e1.id,e2.id)) == 2)
+    assert(run(compositeTable(e1.id)) == None)
+    assert(run(compositeTable(e2.id)) == None)
+    //sneak in a test for accidental deletion
+    assert(run(compositeTable(e3.id)) == Some(e3))
+  }
+
+
+  test("updates of entities with composite key are reflected in future selects") {
+    val e1 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e2 = CompositeKeyEntity(randomCompositeKey, randomString)
+    val e3 = CompositeKeyEntity(randomCompositeKey, randomString)
+    assert(run(compositeTable.insertBatch(e1,e2,e3)) == 3)
+    val update1 = e1.copy(name=randomString)
+    assert(e1 != update1)
+    val update2 = e2.copy(name=randomString)
+    assert(e2 != update2)
+    assert(run(compositeTable.update(update1, update2)) == 2)
+    assert(run(compositeTable(e1.id)) == Some(update1))
+    assert(run(compositeTable(e2.id)) == Some(update2))
+    //sneak in a test for accidental update
+    assert(run(compositeTable(e3.id)) == Some(e3))
+  }
 
 
   test("insertReturningKey of an entity with autogen primary key returns the correct Key and the entity can be selected") (pending)
