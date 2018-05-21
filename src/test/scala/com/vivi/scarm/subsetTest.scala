fdd956bdc48978caea546f13202b2f7205694cef
Bruce Cota <bcota@unicon.net>
2018-05-21 16:05:35 -0400

    added subset tests

--- src/test/scala/com/vivi/scarm/subsetTest.scala
+++ src/test/scala/com/vivi/scarm/subsetTest.scala
@@ -1,12 +1,32 @@
-package com.vivi.scarm
+package com.vivi.scarm.test
 
+import com.vivi.scarm.Subset
+
+import org.scalatest._
 import shapeless._
 
-case class AClass(x: Int, y: String)
-
-object SubsetTest {
+class SubsetTest extends FunSuite {
   val w = Subset[HNil, Int :: HNil]
   val x = Subset[Int, Int :: HNil]
   val y = Subset[Int, String :: Int :: HNil]
   val z = Subset[String :: Int :: HNil, String :: Int :: HNil]
+
+  test("not everything is a subset") {
+
+    assertDoesNotCompile(
+      "val mismatch = Subset[String, Int :: HNil]"
+    )
+
+    assertDoesNotCompile(
+      "val mismatch = Subset[String :: Int, Int :: HNil]"
+    )
+
+    assertDoesNotCompile(
+      "val mismatch = Subset[Int :: String, Float :: Int :: HNil]"
+    )
+
+    assertDoesNotCompile(
+      "val mismatch = Subset[Int, HNil]"
+    )
+  }
 }
